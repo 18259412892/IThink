68d580b11096f35ffd59b8d4c2077580fdcf1183
zhangyunjiang@zhangzhu <58247712+18259412892@users.noreply.github.com>
2021-01-05 00:12:51 +0800

    message提示弹窗类型 (#20)
    * 新增按钮大小属性
    
    * 新增按钮加载效果
    
    * 按钮
    
    * 新增message提示弹窗
    
    * 提示
    
    * ...提示

--- src/packges/message/message.tsx
+++ src/packges/message/message.tsx
@@ -5,8 +5,21 @@ import { createNamespace } from '../utils';
 import { BORDER_SURROUND } from '../utils/constant';
 // Components
 const [createComponent, bem] = createNamespace('message');
-export default function Message(options:{}){
-  const _msg = {
+interface optionsProps {
+  type?: string
+  timeout?: number//2500,
+  right?: boolean
+  content?: string// = "" || _msg[type],
+  position?: string// = right ? 'right': 'center',
+  showClose?: boolean
+  closeAll?: boolean
+}
+interface iconProps{
+  [index:string]:string
+}
+
+export default function Message(options:optionsProps={}){
+  const _msg: iconProps = {
     info: 'info',
     warning: 'warning',
     error: 'error',
@@ -23,13 +36,12 @@ export default function Message(options:{}){
     showClose = false,
     closeAll = false
   } = options;
-  console.log(options,'--options')
   const classes = [
     bem([
       `${type}`
     ])
   ];
-  let _icon = {
+  let _icon:iconProps = {
     info: '<svg width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M24 44C29.5228 44 34.5228 41.7614 38.1421 38.1421C41.7614 34.5228 44 29.5228 44 24C44 18.4772 41.7614 13.4772 38.1421 9.85786C34.5228 6.23858 29.5228 4 24 4C18.4772 4 13.4772 6.23858 9.85786 9.85786C6.23858 13.4772 4 18.4772 4 24C4 29.5228 6.23858 34.5228 9.85786 38.1421C13.4772 41.7614 18.4772 44 24 44Z" fill="#1890ff" stroke="#1890ff" stroke-width="4" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M24 11C25.3807 11 26.5 12.1193 26.5 13.5C26.5 14.8807 25.3807 16 24 16C22.6193 16 21.5 14.8807 21.5 13.5C21.5 12.1193 22.6193 11 24 11Z" fill="#FFF"/><path d="M24.5 34V20H23.5H22.5" stroke="#FFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M21 34H28" stroke="#FFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>',
     warning: '<svg width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M24 44C29.5228 44 34.5228 41.7614 38.1421 38.1421C41.7614 34.5228 44 29.5228 44 24C44 18.4772 41.7614 13.4772 38.1421 9.85786C34.5228 6.23858 29.5228 4 24 4C18.4772 4 13.4772 6.23858 9.85786 9.85786C6.23858 13.4772 4 18.4772 4 24C4 29.5228 6.23858 34.5228 9.85786 38.1421C13.4772 41.7614 18.4772 44 24 44Z" fill="#faad14" stroke="#faad14" stroke-width="4" stroke-linejoin="round"/><path fill-rule="evenodd" clip-rule="evenodd" d="M24 37C25.3807 37 26.5 35.8807 26.5 34.5C26.5 33.1193 25.3807 32 24 32C22.6193 32 21.5 33.1193 21.5 34.5C21.5 35.8807 22.6193 37 24 37Z" fill="#FFF"/><path d="M24 12V28" stroke="#FFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>',
     error: '<svg width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M24 44C35.0457 44 44 35.0457 44 24C44 12.9543 35.0457 4 24 4C12.9543 4 4 12.9543 4 24C4 35.0457 12.9543 44 24 44Z" fill="#f5222d" stroke="#f5222d" stroke-width="4" stroke-linejoin="round"/><path d="M29.6569 18.3431L18.3432 29.6568" stroke="#FFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M18.3432 18.3431L29.6569 29.6568" stroke="#FFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>',
@@ -38,9 +50,9 @@ export default function Message(options:{}){
     close: '<svg width="16" height="16" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M14 14L34 34" stroke="#909399" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M14 34L34 14" stroke="#909399" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>'
   };
   let _options =  {
-    create(body){
+    create(body:any){
       let dom = document.createElement('div');
-      body.appendChild(contentDom);
+      body.appendChild(dom);
     },
     _html(){
       let _html = `
@@ -54,7 +66,7 @@ export default function Message(options:{}){
     },
     dom(){
       let dom = document.createElement('div');
-      dom.className= classes;
+      dom.className = classes;
       let qmsgItem = document.createElement('div');
       let contentDom = document.createElement('div');
       qmsgItem.className = 'qmsg-item';
@@ -77,8 +89,8 @@ export default function Message(options:{}){
     removeFn(){
       // this.doms.remove();
       let _colse = this.doms.querySelectorAll('.qmsg-icon-close');
-      _colse.forEach(function(item,index){
-        item.addEventListener('click',function(ev){
+      _colse.forEach(function(item:any,index:number){
+        item.addEventListener('click',function(ev:any){
           let _name = ev.target;
           let _dom = _name.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
           let num = 1;
@@ -93,7 +105,7 @@ export default function Message(options:{}){
         })
       })
     },
-    remove(dom){
+    remove(dom:any){
       let num = 1;
      const Imte = setTimeout(() => {
         if(dom){
@@ -132,7 +144,7 @@ export default function Message(options:{}){
               item.remove();
             }
             num = num - .3
-            item.style.opacity = num
+            item.style.opacity= num
           },80)
         })
       }
