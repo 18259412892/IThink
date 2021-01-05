9e76fef7c47d99b3a60e027b6e2165d261138cfc
zhangyunjiang@zhangzhu <58247712+18259412892@users.noreply.github.com>
2021-01-05 22:34:23 +0800

    Release vue3.0框架结构搭建 1224 (#24)
    * 新增按钮大小属性
    
    * 新增按钮加载效果
    
    * 按钮
    
    * 新增message提示弹窗
    
    * 提示
    
    * ...提示
    
    * 添加路由
    
    * msg提示窗
    
    * 调整边距
    
    * 解决message报错问题

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="nav">
-    <router-link to="/button">button</router-link>
-    <router-link  to="/message">message</router-link>
+    <router-link to="/button" class="btns">button</router-link>
+    <router-link  to="/message" class="btns">message</router-link>
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-
+  #nav{
+      margin: 10px;
+    .btns{
+      margin: 5px;
+    }
+  }
 </style>
